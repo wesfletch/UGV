1c87bcfaa2e2d369709e5d1fed0aa8b55ac6dd07
Wes Fletcher <wfletcher@geisel-software.com>
2022-09-07 09:27:12 -0400

    OOP-ifies and migrates to ROS2

--- ugv_sim/ugv_sim/scripts/motors.py
+++ ugv_sim/ugv_sim/scripts/motors.py
@@ -1,109 +1,134 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-''' allows teleoperation of a simulation vehicle with the arrow keys
+''' Allows teleoperation of a simulation vehicle with the arrow keys
     assumes differential drive control, and that the base of the robot listens on /cmd_vel
     publishes Twist messages'''
 
-import rospy
+import sys
+from typing import Dict, List
 from pynput import keyboard
-from geometry_msgs.msg import Twist
 
-motor_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=100)  # creating publisher for topic /cmd_vel (that the robot subscribes to)
-
-# Twist message to be published, modified by on_press and on_release
-twist = Twist()
-twist.linear.y = 0
-twist.linear.z = 0
-twist.angular.x = 0
-twist.angular.y = 0
-
-# tells us when the Twist message needs to be published again
-change_flag = False
-
-def on_press(key):
-    ''' Every time a key is pressed, create and publish a twist message (if key pressed was an arrow key) '''
-
-    global change_flag
-
-    try:
-        k = key.char
-    except AttributeError:
-        k = key.name
-
-    # modify twist message depending on which arrow key pressed    
-    if( k == 'left' ):
-        twist.angular.z = 1.2
-        change_flag = True
-    if( k == 'right' ):
-        twist.angular.z = -1.2
-        change_flag = True
-    if( k == 'up' ):
-        twist.linear.x = 1.2
-        change_flag = True
-    if( k == 'down' ):
-        twist.linear.x = -1.2
-        change_flag = True      
-
-    # update published twist message
-    update()    
-
-def on_release(key):
-    ''' When a key is released, zero that key out in the Twist message '''    
-
-    global change_flag
-
-    try:
-        k = key.char
-    except AttributeError:
-        k = key.name
-
-    # zero out the field of whatever key was released    
-    if( k == 'left' ):
-        twist.angular.z = 0
-        change_flag = True
-    if( k == 'right' ):
-        twist.angular.z = 0
-        change_flag = True
-    if( k == 'up' ):
-        twist.linear.x = 0
-        change_flag = True
-    if( k == 'down' ):
-        twist.linear.x = 0 
-        change_flag = True 
-
-    # if Escape is pressed, exit keyboard listener
-    if key == keyboard.Key.esc:
-        # Stop listener
-        return False    
-
-    # update published twiest message
-    update()
+import rclpy
+from rclpy.node import Node
+import geometry_msgs.msg as geom
 
 
-def update():
+class MotorsPub(rclpy.node.Node):
 
-    global change_flag
-
-    # if a new key was pressed or released, update the twist message
-    if (change_flag):
-        motor_pub.publish(twist)
-        change_flag = False
+    def __init__(self, topic: str = '/cmd_vel', freq: int = 50, speeds: List[float] = [1.2,-1.2,1.2,-1.2]) -> None:
+        """A ROS2 node that publishes motor commmands on provided topic at provided freq based on keyboard inputs.
         
+        Intended to control differential drive vehicles (in sim, mostly).
+
+        Args:
+            topic (str, optional): The topic to publish geometry_msgs/Twist messages on. Defaults to '/cmd_vel'.
+            freq (int, optional): Frequenct to publish messages at. Defaults to 50.
+            speeds (List[float], optional): List of motor speeds. Format is [FWD,REV,CW,CCW]. Defaults to [1.2,-1.2,1.2,-1.2].
+        """
+
+        super().__init__(node_name='motor_publisher')
+        
+        self.publisher: rclpy.Publisher = self.create_publisher(
+            msg_type=geom.Twist, 
+            topic=topic, 
+            qos_profile=10)
+        
+        self.speeds: Dict[str, float] = {
+            'FORWARD': speeds[0],
+            'REVERSE': speeds[1],
+            'CW'     : speeds[2],
+            'CCW'    : speeds[3],
+        }
+
+        # message to be published
+        self.twist: geom.Twist = geom.Twist()
+        self.twist.linear.x = 0.0
+        self.twist.linear.y = 0.0
+        self.twist.linear.z = 0.0
+        self.twist.angular.x = 0.0
+        self.twist.angular.y = 0.0
+        self.twist.angular.z = 0.0
+
+        # timer that fires at provided frequency
+        self._timer_period: float = 1 / freq
+        self.timer: rclpy.Timer = self.create_timer(self._timer_period, self.timer_callback)
+
+        # listener for keyboard press/release events
+        self.listener: keyboard.Listener = keyboard.Listener(
+            on_press=self.on_press_cb,
+            on_release=self.on_release_cb)
+        self.listener.start()
+
+    def timer_callback(self) -> None:
+        """Publishes the Twist message created by listening to keyboard inputs.
+
+        Fires at the frequency provided to the constructor.
+        """
+        
+        self.publisher.publish(self.twist)
+        
+    def on_press_cb(self, key: keyboard.Key) -> bool:
+        """When a key is pressed, modify the Twist message that is published by this node.
+
+        Args:
+            key (keyboard.Key): The key code that caused this callback to fire.
+
+        Returns:
+            bool: Returns false to stop listener (when ESC is pressed)
+        """
+
+        try:
+            k = key.char
+        except AttributeError:
+            k = key.name
+
+        # modify twist message depending on which arrow key was pressed    
+        if k == 'left':
+            self.twist.angular.z = self.speeds['CCW']
+        if k == 'right':
+            self.twist.angular.z = self.speeds['CW']
+        if k == 'up':
+            self.twist.linear.x = self.speeds['FORWARD']
+        if k == 'down':
+            self.twist.linear.x = self.speeds['REVERSE']
+
+        # if Escape is pressed, exit keyboard listener
+        if key == keyboard.Key.esc:
+            # Stop listener
+            return False      
+        
+        return True
+
+    def on_release_cb(self, key: keyboard.Key) -> None:
+        """When a key is released, zero that key out in the Twist message
+
+        Args:
+            key (keyboard.Key): The key code that caused this callback to fire.
+        """
+
+        try:
+            k = key.char
+        except AttributeError:
+            k = key.name
+
+        # zero out the field of whatever key was released    
+        if k == 'left':
+            self.twist.angular.z = 0.0
+        if k == 'right':
+            self.twist.angular.z = 0.0
+        if k == 'up':
+            self.twist.linear.x = 0.0
+        if k == 'down':
+            self.twist.linear.x = 0.0  
 
 def main():
 
-    # TODO: add CTRL+C signal handler. 
+    rclpy.init(args=sys.argv) 
 
-    rospy.init_node('motor_controller', anonymous=True) # node that will handle sending commands
-    listener = keyboard.Listener()  # pynput keyboard listener for catching arrow key input
-
-    # when key is pressed, run on_press(), when a key is released, run on_release()
-    with keyboard.Listener(
-        on_press=on_press,  
-        on_release=on_release) as listener: 
-        listener.join()
-
-    rospy.spin()    # keeps this ros node from closing
+    motors_pub = MotorsPub()
+    rclpy.spin(motors_pub)    # keeps this ros node from closing
+    
+    rclpy.shutdown()
 
 if __name__=='__main__':
     main()
